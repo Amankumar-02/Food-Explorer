0322d0b184dbf9964be2bb33285e5bb9f9bd405e
Aman <ak193566@gmail.com>
2024-02-21 11:55:09 +0530

    build a react project from scratch
    change the normal index.html, index.css, App.js files to React project

---
+++ vite_02/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+    React.createElement("div", {id:"header"}, [
+        React.createElement("div", {id:"child1"}, [
+            React.createElement("h1", {}, "I'm an h1 Tag Namaste React develop using vite from child 1"),
+            React.createElement("h2", {}, "I'm an h2 Tag Namaste React develop using vite from child 1")
+        ]),
+        React.createElement("div", {id:"child2"}, [
+            React.createElement("h1", {}, "I'm an h1 Tag from child 2"),
+            React.createElement("h2", {}, "I'm an h2 Tag from child 2")
+        ])
+    ])
+)
