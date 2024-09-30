392f93d2daf314ddb70b4a011e373977cbbc6fd8
Aman <ak193566@gmail.com>
2024-09-30 16:32:20 +0530

    update

--- src/components/MenuItems/MenuCategory.jsx
+++ src/components/MenuItems/MenuCategory.jsx
@@ -13,25 +13,30 @@ function MenuCategory({card, id, toggleEvent, showToggle, restName, restId}) {
   // }
   return (
     <>
-      <div className="w-[80%] transition-all">
-        <div className="flex justify-between items-center mt-8 p-4 shadow-md bg-gray-300 rounded-xl" onClick={()=>{toggleEvent(id)}}>
-          <h1 className="text-2xl font-semibold">
-            {card?.card?.title} ({card?.card?.itemCards.length})
-          </h1>
-          <button
-            className="text-lg font-semibold px-2 rounded-xl"
-          >
-            {/* {toggleEvent.name} */}
-            {showToggle? (<><i className="ri-arrow-up-s-line text-xl"></i></>) : (<><i className="ri-arrow-down-s-line text-xl"></i></>)}
-          </button>
-        </div>
-        {/* <div className="" style={toggle}></div> */}
-        <div className={`${showToggle? ("block"): ("hidden")}`}>
-          {card?.card?.itemCards.map(({ card }, index2) => (
-            <MenuItem key={index2} card={card} restName={restName} restId={restId}/>
-          ))}
-        </div>
-      </div>
+      <div className="w-full transition-all">
+  <div 
+    className="flex justify-between items-center mt-4 lg:mt-8 p-2 lg:p-4 shadow-md bg-gray-300 rounded-xl cursor-pointer"
+    onClick={() => { toggleEvent(id); }}
+  >
+    <h1 className="text-lg md:text-2xl font-semibold flex-1">
+      {card?.card?.title} ({card?.card?.itemCards.length})
+    </h1>
+    <button className="text-lg font-semibold px-2 rounded-xl mt-2 md:mt-0">
+      {showToggle ? (
+        <i className="ri-arrow-up-s-line text-xl"></i>
+      ) : (
+        <i className="ri-arrow-down-s-line text-xl"></i>
+      )}
+    </button>
+  </div>
+
+  <div className={`${showToggle ? "block" : "hidden"}`}>
+    {card?.card?.itemCards.map(({ card }, index2) => (
+      <MenuItem key={index2} card={card} restName={restName} restId={restId} />
+    ))}
+  </div>
+</div>
+
     </>
   );
 }
--- src/components/MenuItems/MenuItem.jsx
+++ src/components/MenuItems/MenuItem.jsx
@@ -26,53 +26,63 @@ function MenuItem({card, restName, restId}) {
 
   return (
     <>
-      <div className="flex justify-between items-center py-6 px-8 border border-x-0 hover:bg-gray-200 rounded-xl">
-        <ul className="flex flex-col gap-4 justify-center list-none w-[80%]">
-          {card?.info?.itemAttribute?.vegClassifier === "VEG" ? (
-            <li className="w-fit border border-green-500 text-[8px]">🟢</li>
-          ) : null}
-          {card?.info?.itemAttribute?.vegClassifier === "NONVEG" ? (
-            <li className="w-fit border border-red-500 text-[8px]">🔺</li>
-          ) : null}
-          <div>
-            <li className="text-lg font-bold">{card?.info?.name}</li>
-            <li className="text-lg font-bold text-gray-600">
-              ₹{card?.info?.price / 100 || card?.info?.defaultPrice / 100}
-            </li>
-          </div>
-          <li className="leading-4 text-sm">{card?.info?.description}</li>
-        </ul>
-        <div className="relative w-[150px] h-[120px] flex justify-center">
-          {card?.info?.imageId ? (
-            <img
-              src={MENU_IMG_URL + card?.info?.imageId}
-              className="w-full h-full rounded-xl object-cover hover:scale-[1.08] transition-all"
-            />
-          ) : null}
-          <div className="absolute bottom-[-10%] bg-white z-[99] flex border-2 rounded-xl overflow-hidden">
+      <div className="flex justify-between items-center p-3 lg:p-6 my-2 lg:my-4 border border-x-0 hover:bg-gray-200 rounded-xl gap-4 lg:gap-10">
+  <ul className="flex flex-col gap-2 lg:gap-4 py-2 lg:py-4 justify-center list-none w-full flex-1">
+    <div>
+    <div className='flex items-center gap-2'>
+    {card?.info?.itemAttribute?.vegClassifier === "VEG" ? (
+      <li className="w-fit border border-green-500 text-[8px]">🟢</li>
+    ) : null}
+    {card?.info?.itemAttribute?.vegClassifier === "NONVEG" ? (
+      <li className="w-fit border border-red-500 text-[8px]">🔺</li>
+    ) : null}
+    <li className='text-sm lg:text-base text-orange-500 font-semibold'>{card?.info?.isBestseller ? "Bestseller" : null}</li>
+    </div>
+      <li className="text-base lg:text-lg font-bold">{card?.info?.name}</li>
+      <li className="text-base lg:text-lg font-bold text-gray-600">
+        ₹{card?.info?.price / 100 || card?.info?.defaultPrice / 100}
+      </li>
+    </div>
+      <li className="text-base lg:text-lg font-bold text-gray-600">
+      <i className="ri-star-fill text-yellow-500 text-lg"></i>{" "}{card?.info?.ratings?.aggregatedRating?.rating}{" "} <span className='text-sm lg:text-md'>({card?.info?.ratings?.aggregatedRating?.ratingCountV2})</span>
+      </li>
+    <li className="leading-4 text-xs lg:text-sm">{card?.info?.description}</li>
+  </ul>
+  <div className="relative w-[100px] lg:w-[150px] h-[100px] lg:h-[150px] flex justify-center">
+    {card?.info?.imageId ? (
+      <img
+        src={MENU_IMG_URL + card?.info?.imageId}
+        className="w-full h-full rounded-xl object-cover hover:scale-[1.08] transition-all"
+      />
+    ) : null}
 
-            {storeData.filter(item=>item?.info?.name===card?.info?.name)[0]?.info?.quantity > 0? (
-              <>
-                <button className="px-2 hover:bg-gray-300" onClick={()=>{modifyQuantity("decrease", card?.info?.name)}}>-</button>
-              </>
-            ) : null}
-            
-            <button
-              className="px-2 hover:bg-green-500 hover:text-white"
-              onClick={()=>{cartEvent(card?.info?.name)}}
-            >
-              {storeData.filter(item=>item?.info?.name===card?.info?.name)[0]?.info?.quantity > 0? (<>{storeData.filter(item=>item?.info?.name===card?.info?.name)[0]?.info?.quantity}</>) : (<>Add</>)}
-            </button>
 
-            {storeData.filter(item=>item?.info?.name===card?.info?.name)[0]?.info?.quantity > 0? (
-              <>
-                <button className="px-2 hover:bg-gray-300" onClick={()=>{modifyQuantity("increase", card?.info?.name)}}>+</button>
-              </>
-            ) : null}
-            
-          </div>
-        </div>
-      </div>
+    <div className="absolute bottom-0 lg:bottom-[-10%] bg-white z-[99] flex border-2 rounded-xl overflow-hidden">
+      {storeData.filter(item => item?.info?.name === card?.info?.name)[0]?.info?.quantity > 0 ? (
+        <>
+          <button className="px-2 hover:bg-gray-300" onClick={() => { modifyQuantity("decrease", card?.info?.name) }}>-</button>
+        </>
+      ) : null}
+      
+      <button
+        className="px-2 hover:bg-green-500 hover:text-white"
+        onClick={() => { cartEvent(card?.info?.name) }}
+      >
+        {storeData.filter(item => item?.info?.name === card?.info?.name)[0]?.info?.quantity > 0 ? (
+          <>{storeData.filter(item => item?.info?.name === card?.info?.name)[0]?.info?.quantity}</>
+        ) : (
+          <>Add</>
+        )}
+      </button>
+
+      {storeData.filter(item => item?.info?.name === card?.info?.name)[0]?.info?.quantity > 0 ? (
+        <>
+          <button className="px-2 hover:bg-gray-300" onClick={() => { modifyQuantity("increase", card?.info?.name) }}>+</button>
+        </>
+      ) : null}
+    </div>
+  </div>
+</div>
     </>
   );
 }
